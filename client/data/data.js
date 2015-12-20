e4b514536ae617e8d37082ae94ae6ec415c471fb
jguardado <jguardado85@gmail.com>
2015-12-19 17:04:23 -0800

    player is visible

--- client/data/data.js
+++ client/data/data.js
@@ -18,24 +18,24 @@
 
 */
 var songData = [
-  new SongModel({
+  {
     url: "https://s3-us-west-1.amazonaws.com/hr-mytunes/data/04+One+In+A+Million.mp3",
     title: "One In A Million",
     artist: "Aaliyah",
-  }),
-  new SongModel({
+  },
+  {
     url: "https://s3-us-west-1.amazonaws.com/hr-mytunes/data/03+Age+Ain%27t+Nothing+But+A+Number.mp3",
     title: "Age Ain't Nothing But A Number",
     artist: "Aaliyah",
-  }),
-  new SongModel({
+  },
+  {
     url: "https://s3-us-west-1.amazonaws.com/hr-mytunes/data/05+Hot+Like+Fire.mp3",
     title: "Hot Like Fire",
     artist: "Aaliyah",
-  }),
-  new SongModel({
+  },
+  {
     url: "https://s3-us-west-1.amazonaws.com/hr-mytunes/data/06+If+Your+Girl+Only+Knew.mp3",
     title: "If Your Girl Only Knew",
     artist: "Aaliyah",
-  })
+  }
 ];
